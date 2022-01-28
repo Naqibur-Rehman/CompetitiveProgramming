4816abaca2e4f240bc27b9955ce095183e3afb15
Naqibur Rehman <55501073+Naqibur-Rehman@users.noreply.github.com>
2022-01-28 17:22:38 +0530

    Create Pattern6.java
    *	*	*		*	*	*	
    *	*				*	*	
    *						*	
    *	*				*	*	
    *	*	*		*	*	*

---
+++ Pattern6.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int space = 1;
+        int star = n/2 + 1;
+        
+        for(int i = 1; i<= n; i++){
+            
+            for(int j=1; j<=star; j++){
+                System.out.print("*\t");
+            }
+            for(int j=1; j<=space; j++){
+                System.out.print("\t");
+            }
+            for(int j=1; j<=star; j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+            if(i <= n/2){
+                space += 2;
+                star--;
+            } else {
+                space -= 2;
+                star++;
+                
+            }
+        }
+        
+    }
+}
